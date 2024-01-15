7e22222932c90dd20ae950c3aa7a7b8a92e15b58
youznn <leejin914418@gmail.com>
2024-01-15 20:53:39 +0900

    feat: init setting

---
+++ frontend/app/(root)/_components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="flex w-dvw flex-row bg-lime-600">Header</div>;
+}
---
+++ frontend/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/trade">trade </Link>
+      <Link href="/friend">friend </Link>
+      <div>home</div>
+    </div>
+  );
+}
